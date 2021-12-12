27d7dc749fb9e3519bb0fd50f9be0b22062ca440
Snazzah <suggesttosnazzy@gmail.com>
2021-12-12 16:34:33 -0600

    feat: fix events and add status change on ready

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,5 @@
 import Eris from 'eris';
-import { onGuildLeave, onWebhooksUpdate } from './events';
+import { onGuildJoin, onGuildLeave, onWebhooksUpdate } from './events';
 import { logger } from './logger';
 
 export const client = new Eris.Client(process.env.DISCORD_TOKEN, {
@@ -10,11 +10,15 @@ export const client = new Eris.Client(process.env.DISCORD_TOKEN, {
 });
 
 // Events
-client.on('ready', () => logger.info('All shards ready.'));
+client.on('ready', () => {
+  logger.info('All shards ready.');
+  client.editStatus('online', { name: 'boards scroll by me', type: 3 });
+});
 client.on('disconnect', () => logger.warn('All shards Disconnected.'));
 client.on('reconnecting', () => logger.warn('Reconnecting client.'));
 client.on('debug', (message) => logger.debug(message));
 
+client.on('guildCreate', onGuildJoin);
 client.on('guildDelete', onGuildLeave);
 client.on('webhooksUpdate', onWebhooksUpdate);
 
